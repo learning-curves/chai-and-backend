311f590f7f9b942bd8a2221e4a4dffae59058708
Ahmed Farooq Nasir <afnsafns@yahoo.com>
2026-02-28 23:34:05 +0500

    add cors and cookie parser

--- src/index.js
+++ src/index.js
@@ -2,27 +2,13 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
 
-/*
-import express from "express";
-
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("ERR".error);
-      throw error;
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is runing : ${process.env.PORT}`);
     });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`App is running on ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.log("ERROR", error);
-    throw err;
-  }
-})();
-*/
+  })
+  .catch((err) => {
+    console.log(`mongodb connection failed: ${err}`);
+  });
